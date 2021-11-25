4fc17780c6e2cc19a3b7a1d12ccabd7702697f87
Jhun <wnsx0000@gmail.com>
2021-11-25 23:47:22 +0900

    mymkfs 구현, 쉘 수정 완료

--- src/mainfile.c
+++ src/mainfile.c
@@ -3,8 +3,8 @@
 
 
 //작업 디렉토리 경로를 저장할 linked list
-DIR_LIST *front_dir_list_ptr;
-DIR_LIST *rear_dir_list_ptr;
+DIR_LIST *front_dir_list_ptr = NULL;
+DIR_LIST *rear_dir_list_ptr = NULL;
 
 
 /*
@@ -16,7 +16,7 @@ DIR_LIST *rear_dir_list_ptr;
 */
 int main(void)
 {
-    //linked list 초기화(move. 나중에 mymkfs() 함수로 옮기기)
+    //pwd linked list 생성
     front_dir_list_ptr = (DIR_LIST *)malloc(sizeof(DIR_LIST));
     front_dir_list_ptr->inode = 1;
     front_dir_list_ptr->name = "/";
@@ -50,10 +50,26 @@ void shell(void)
     //쉘 출력, 명령어 처리
     while(1)
     {
+        //myfs 파일 존재여부 확인
+        int exist = 0; //파일이 존재하지 않는 경우 0, 존재하는 경우 1 저장
+        FILE *test;
+        if((test = fopen("myfs", "rb")) != NULL) //파일이 존재하지 않는 경우
+        {
+            exist = 1;
+        }
+        fclose(test);
+
         //쉘 출력
-        printf("[");
-        mypwd(); //mypwd() 함수로 경로 출력
-        printf(" ]$ ");
+        if(exist == 0) //myfs 파일이 존재하지 않는 경우
+        {
+            printf("$ ");
+        }
+        else //myfs 파일이 존재하는 경우
+        {
+            printf("[");
+            mypwd(); //mypwd() 함수로 경로 출력
+            printf(" ]$ ");
+        }
 
         //포인터가 가리키는 메모리 공간 초기화
         for(int i = 0; i < COM_SEP_NUM; i++)
@@ -104,78 +120,78 @@ void shell(void)
         //명령어 함수 호출
         if(!strcmp("mymkfs", *(com_tmp_ptr)))
         {
-            //mymkfs 함수
+            mymkfs();
         }
-        else if(!strcmp("myls", *(com_tmp_ptr)))
+        else if((!strcmp("myls", *(com_tmp_ptr))) && (exist == 1))
         {
             //myls 함수
         }
-        else if(!strcmp("mycat", *(com_tmp_ptr)))
+        else if((!strcmp("mycat", *(com_tmp_ptr))) && (exist == 1))
         {
             //mycat 함수
         }
-        else if(!strcmp("myshowfile", *(com_tmp_ptr)))
+        else if((!strcmp("myshowfile", *(com_tmp_ptr))) && (exist == 1))
         {
             //myshowfile 함수
         }
-        else if(!strcmp("mypwd", *(com_tmp_ptr)))
+        else if((!strcmp("mypwd", *(com_tmp_ptr))) && (exist == 1))
         {
             mypwd();
             printf("\n");
         }
-        else if(!strcmp("mycd", *(com_tmp_ptr)))
+        else if((!strcmp("mycd", *(com_tmp_ptr))) && (exist == 1))
         {
             //mycd 함수
         }
-        else if(!strcmp("mycp", *(com_tmp_ptr)))
+        else if((!strcmp("mycp", *(com_tmp_ptr))) && (exist == 1))
         {
             //mycp 함수
         }
-        else if(!strcmp("mycpto", *(com_tmp_ptr)))
+        else if((!strcmp("mycpto", *(com_tmp_ptr))) && (exist == 1))
         {
             //mycpto 함수
         }
-        else if(!strcmp("mycpfrom", *(com_tmp_ptr)))
+        else if((!strcmp("mycpfrom", *(com_tmp_ptr))) && (exist == 1))
         {
             //mycpfrom 함수
         }
-        else if(!strcmp("mymkdir", *(com_tmp_ptr)))
+        else if((!strcmp("mymkdir", *(com_tmp_ptr))) && (exist == 1))
         {
             //mymkdir 함수
         }
-        else if(!strcmp("myrmdir", *(com_tmp_ptr)))
+        else if((!strcmp("myrmdir", *(com_tmp_ptr))) && (exist == 1))
         {
             //myrmdir 함수
         }
-        else if(!strcmp("myrm", *(com_tmp_ptr)))
+        else if((!strcmp("myrm", *(com_tmp_ptr))) && (exist == 1))
         {
             //myrm 함수
         }
-        else if(!strcmp("mymv", *(com_tmp_ptr)))
+        else if((!strcmp("mymv", *(com_tmp_ptr))) && (exist == 1))
         {
             //mymv 함수
         }
-        else if(!strcmp("mytouch", *(com_tmp_ptr)))
+        else if((!strcmp("mytouch", *(com_tmp_ptr))) && (exist == 1))
         {
             //mytouch 함수
         }
-        else if(!strcmp("myinode", *(com_tmp_ptr)))
+        else if((!strcmp("myinode", *(com_tmp_ptr))) && (exist == 1))
         {
             myinode(*(com_tmp_ptr + 1));
         }
-        else if(!strcmp("mydatablock", *(com_tmp_ptr)))
+        else if((!strcmp("mydatablock", *(com_tmp_ptr))) && (exist == 1))
         {
             mydatablock(*(com_tmp_ptr + 1));
         }
-        else if(!strcmp("mystate", *(com_tmp_ptr)))
+        else if((!strcmp("mystate", *(com_tmp_ptr))) && (exist == 1))
         {
             //mystate 함수
         }
-        else if(!strcmp("mytree", *(com_tmp_ptr)))
+        else if((!strcmp("mytree", *(com_tmp_ptr))) && (exist == 1))
         {
             mytree(*(com_tmp_ptr + 1));
         }
-        else if(!strcmp("command", *(com_tmp_ptr)))
+        else if((!strcmp("command", *(com_tmp_ptr))) && (exist == 1))
         {
             command();
         }
@@ -183,6 +199,10 @@ void shell(void)
         {
             printf("Bye....\n\n");
             return;
+        }
+        else if(exist == 0)
+        {
+            printf("파일 시스템이 존재하지 않습니다.\n");
         }
         else
         {
