4ee64d94a58ee1de4641a6afd8f18ad944fcf0f6
wj0017 <80533304+wj0017@users.noreply.github.com>
2021-12-02 16:50:48 +0900

    myls

--- src/jin.c
+++ src/jin.c
@@ -28,137 +28,119 @@ int stringtoint(char *byte)
     return n;
 }
 
+
 /*
 이름    : myls 함수
 작성자  : 이우진
 기능    : 파일 목록 자세히 보기
-받는값  : X
+받는값  : 파일 명
 리턴값  : X
 */
-void myls(const char *ptr)
+void myls(const char* file)
 {
-    int inode = 0;  // inode 번호
-    int num = 0; // ptr이 가리키고 있는 문자열의 자릿수
-    int ptr_a = 0;
-
-    while (1)
-    {
-        if (*(ptr + ptr_a) == 0)
-        {
-            break;
-        }
-        else
-        {
-            if ((*(ptr + ptr_a) < 48) || (*(ptr + ptr_a) > 57)) // 정수 문자가 아닌 문자가 있는 경우
-            {
-                printf("inode 번호가 잘못되었습니다.\n");
-                return;
-            }
-            else
-            {
-                ptr_a++;
-                num++;
-            }
-        }
-    }
-
-    ptr_a = 0; // 변수 재사용을 위해 초기화
-
-    for (int i = (num - 1); i >= 0; i--)
-    {
-        inode += (*(ptr + ptr_a) - 48) * (int_pow(10, i));
-
-        ptr_a;
-    }
-
-    if ((inode < 1) || (inode > 128)) // 1~128인 정수가 아닌 경우
-    {
-        printf("inode 번호가 잘못되었습니다.\n");
-        return;
-    }
-
     // 파일 열기
     FILE *myfs;
     myfs = fopen("myfs", "rb");
 
-    if(myfs == NULL)
-    {
-        printf("파일 열기에 실패했습니다.\n");
-        abort();
-    }
+    //현재 디렉토리에서 인자로 입력받은 디렉토리명의 inode 번호 찾기
 
-    // superblock 정보 읽기
-    SUPERBLOCK *sb_ptr = (SUPERBLOCK *)malloc(sizeof(SUPERBLOCK));
-    fseek(myfs, BOOT_BLOCK_SIZE, SEEK_SET);
-    fread(sb_ptr, sizeof(SUPERBLOCK), 1, myfs);
+    int saveinode, count, saveinumber;
+    int *fileinode = (int *)malloc(sizeof(int));
+    saveinode = rear_dir_list_ptr->inode;
 
-    // inode 사용 여부 확인하기
-    unsigned mask;
+    INODE *presenti_data = (INODE *)malloc(sizeof(INODE));
+    fseek(myfs, BOOT_BLOCK_SIZE + SUPER_BLOCK_SIZE + 20 * (saveinode - 1), SEEK_SET);
+    fread(presenti_data, sizeof(INODE), 1, myfs);
 
-    if (inode < 33) // inode_1에 정보가 들어있는 경우
+    int n = presenti_data->size / (8 + sizeof(int));
+    char *filename = (char *)malloc(sizeof(char) * 8);
+
+    int none_tmp = 0;
+
+    fseek(myfs, BOOT_BLOCK_SIZE + SUPER_BLOCK_SIZE + INODE_LIST_SIZE + (DATA_BLOCK_SIZE * (presenti_data->dir_1)), SEEK_SET);
+    for (int i = 0; i < n; i++)
     {
-        mask = 1 << (inode - 1);
-        if ((sb_ptr->inode_1 & mask) == 0)
+        fread(filename, 8, 1, myfs);
+        fread(fileinode, sizeof(int), 1, myfs);
+
+        if (strcmp(file, filename) == 0)
         {
-            printf("해당 inode는 사용 중이 아닙니다.\n");
-            free(sb_ptr);
-            fclose(myfs);
-            return;
+            count = i;
+            break;
         }
-    }
-    else if (inode < 65) // inode_2에 정보가 들어있는 경우
-    {
-        mask = 1 << ((inode - 32) - 1);
-        if ((sb_ptr->inode_2 & mask) == 0)
+        else
         {
-            printf("해당 inode는 사용 중이 아닙니다.\n");
-            free(sb_ptr);
-            fclose(myfs);
-            return;
-        }
-    }
-    else if (inode < 97) // inode_3에 정보가 들어있는 경우
-    {
-        mask = 1 << ((inode - 64) - 1);
-        if ((sb_ptr->inode_3 & mask) == 0)
-        {
-            printf("해당 inode는 사용 중이 아닙니다.\n");
-            free(sb_ptr);
-            fclose(myfs);
-            return;
-        }
-    }
-    else // inode_4에 정보가 들어있는 경우
-    {
-        mask = 1 << ((inode - 96) - 1);
-        if ((sb_ptr->inode_4 & mask) == 0)
-        {
-            printf("해당 inode는 사용 중이 아닙니다.\n");
-            free(sb_ptr);
-            fclose(myfs);
-            return;
+            none_tmp++;
         }
     }
 
-
-    while(inode < 128 + 1 ) // 파일 업데이트 시간, 파일 유형, inode 번호, 파일 크기
+    if (none_tmp == n) //해당 이름의 파일이 존재하지 않는 경우
     {
+        printf("해당 이름의 파일이 존재하지 않습니다.\n");
+
+        return;
+    }
+    else //해당 이름의 파일이 존재하는 경우
+    {
+        //해당 파일이 일반 파일인지 검사
+        INODE *file_inode_tmp_ptr = (INODE *)malloc(sizeof(INODE));
+        fseek(myfs, BOOT_BLOCK_SIZE + SUPER_BLOCK_SIZE + sizeof(INODE) * (*fileinode - 1), SEEK_SET);
+        fread(file_inode_tmp_ptr, sizeof(INODE), 1, myfs);
+        if (file_inode_tmp_ptr->type == 1) //해당 파일이 일반 파일인 경우
+        {
+            printf("해당 파일은 디렉토리가 아닙니다.\n");
+
+            free(file_inode_tmp_ptr);
+            free(fileinode);
+            free(presenti_data);
+            free(filename);
+
+            return;
+        }
+        else //해당 파일이 디렉토리인 경우
+        {
+            free(file_inode_tmp_ptr);
+        }
+    }
+
+    //해당 inode의 datablock 번호 찾기
+
+    INODE *file_d = (INODE *)malloc(sizeof(INODE));
+    fseek(myfs, BOOT_BLOCK_SIZE + SUPER_BLOCK_SIZE + sizeof(INODE) * (*fileinode - 1), SEEK_SET);
+    fread(file_d, sizeof(INODE), 1, myfs);
+    int data_num = (int)(file_d->dir_1 + 1);
+
+
+    // datablock에서 각각의 파일 찾아내고 그 다음 모든 파일의 inode로 세부사항 접근
+
+    n = file_d->size / (8 + sizeof(int));
+
+
+    for(int i = count; i < n; i++)
+    {
+        int i = 0;
+        fseek(myfs, BOOT_BLOCK_SIZE + SUPER_BLOCK_SIZE + INODE_LIST_SIZE + ((DATA_BLOCK_SIZE * (data_num)) + i), SEEK_SET);
+        fread(filename, 8, 1, myfs);
+        fread(fileinode, sizeof(int), 1, myfs);
+
+        // 파일명
+        printf("%s" ,filename);
+
         // inode list 정보 읽기
-        INODE *inode_ptr = (INODE *)malloc(sizeof(INODE)); 
-        fseek(myfs, BOOT_BLOCK_SIZE + SUPER_BLOCK_SIZE + (sizeof(INODE) * (inode - 1)), SEEK_SET);
+        fseek(myfs, BOOT_BLOCK_SIZE + SUPER_BLOCK_SIZE + (sizeof(INODE) * (*fileinode - 1)), SEEK_SET);
+        INODE *inode_ptr = (INODE *)malloc(sizeof(INODE));
         fread(inode_ptr, sizeof(INODE), 1, myfs);
-
 
         // 생성일자
         printf("    생성일자 : ");
         
         if (inode_ptr->year > 50)
         {
-            printf(" %d/", inode_ptr->year);
+            printf(" 19%d/", inode_ptr->year);
         }
         else
         {
-            printf(" %d/", inode_ptr->year);
+            printf(" 20%d/", inode_ptr->year);
         }
         printf("%d/%d ", inode_ptr->month, inode_ptr->date);
         printf("%d:%d:%d", inode_ptr->hour, inode_ptr->minute, inode_ptr->second);
@@ -176,21 +158,20 @@ void myls(const char *ptr)
             printf(" 파일");
         }
 
-
         // inode 번호
-
-        printf("    inode번호 : %d", inode);
+        printf("    inode번호 : %d", inode_ptr);
 
 
         // 크기
         printf("    크기 : %d\n", inode_ptr->size);
 
-        free(inode_ptr);
+        fseek(myfs, -1, SEEK_CUR);
+        fseek(myfs,sizeof(int),SEEK_CUR);
 
-        inode++;
+        i++;
     }
-    
-    free(sb_ptr);
+
+
     fclose(myfs);
 
     return;
